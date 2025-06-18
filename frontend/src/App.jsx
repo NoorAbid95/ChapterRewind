c13f49b230d32edeaace12cc49cbc485ae5092bb
Noor Abid <noorabid95@gmail.com>
2025-06-18 20:17:53 +1000

    Initial navbar setup and temp styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
   );
 }
 
