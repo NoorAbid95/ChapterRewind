6aa5241f1ac3c34dce8c5fdefa5b27798a9ec655
Noor Abid <noorabid95@gmail.com>
2025-07-05 00:14:25 +1000

    Create login auth endpoint and auth middleware

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -42,3 +42,30 @@ export const signup = async (req, res) => {
     sendError(res);
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return sendError(res, "Invalid Credentials", 400);
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return sendError(res, "Invalid Credentials", 400);
+    }
+
+    generateToken(user._id, res);
+    sendSuccess(res, {
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+    });
+  } catch (error) {
+    console.log("Error in Login Controller", error.message);
+    sendError(res);
+  }
+};
