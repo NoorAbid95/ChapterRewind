63d7324abea21749789ae44305125c2f281dabd9
Noor Abid <your.email@example.com>
2025-09-02 22:27:52 +1000

    Fix cors communication issue for deployed app

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,15 +12,12 @@ const app = express();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV !== "production") {
-  app.use(
-    cors({
-      origin: "http://localhost:5173",
-      credentials: true,
-    })
-  );
+const corsOptions = { 
+  origin: process.env.NODE_ENV === 'production' ? "https://chapterrewind.onrender.com" : "http://localhost:5173", 
+  credentials: true
 }
 
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(cookieParser());
 
